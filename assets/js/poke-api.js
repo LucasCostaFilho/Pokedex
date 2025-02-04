2ac6779d7d9269ce75e0f63b6b19d4eccd4f02e6
Lucas dos Santos Costa <512971@pjro.local@FJ082470.pjro.local>
2025-02-04 12:38:08 -0400

    commit inicial

---
+++ assets/js/poke-api.js
@@ -0,0 +1,33 @@
+const pokeApi = {}
+
+function converPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    const types = pokeDetail.types.map((typesSlot) => typesSlot.type.name)
+    const [type] = types
+   
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(converPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 50) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
