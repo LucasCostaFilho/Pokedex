5f123a48e7a9a37284fd234353449451e831bea0
Lucas dos Santos Costa <512971@pjro.local@FJ082470.pjro.local>
2025-02-06 12:14:27 -0400

    comeco do modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const modalButton = document.getElementById('modalButton')
+const modal = document.getElementById('modal')
+const buttonClose = document.getElementById('buttonClose')
 
 const maxRecords = 151
 const limit = 10;
@@ -8,16 +11,18 @@ let offset = 0;
 function loadPokemonsItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         pokemonList.innerHTML += pokemons.map((pokemon) => `
-    <li class="pokemon ${pokemon.type}">
-                    <span class="number">#${pokemon.number}</span>
-                    <span class="name">${pokemon.name}</span>
-                    <div class="detail">
-                        <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                        </ol>
-                        <img src="${pokemon.photo}" alt="${pokemon.name}">
-                    </div>
-                </li>
+        <button class="pokemonButton">
+            <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+        </button>
     `).join('')
     })
 }
@@ -38,3 +43,11 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonsItens(offset, limit)
     }
 })
+
+modalButton.onclick = () => {
+    modal.showModal()
+}
+
+buttonClose.onclick = () => {
+    modal.close()
+}
